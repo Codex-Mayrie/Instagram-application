4c711cf0961e8d059051d831dd5b5037fec1549c
shee1998 <marywanjikumburu1@gmail.com>
2020-01-03 12:58:16 +0300

    create application files

--- Insta/views.py
+++ Insta/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def registration(request):
+  if request.method == 'POST'
+  form = RegistrationForm(request.POST)
+  if form.is_valid():
+    form.save()
+  return redirect('/login')
+else:
+  form = RegisterForm()
+return render(request, 'registration/sign-up.html', {"form": form})
