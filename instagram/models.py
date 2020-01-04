7e81397ba68e9235ba727d02c50753603156aad1
shee1998 <marywanjikumburu1@gmail.com>
2020-01-04 23:40:05 +0300

    create Image Model

--- instagram/models.py
+++ instagram/models.py
@@ -26,3 +26,56 @@ class Profile(models.Model):
 def get_by_id(cls, id):
   user_images = Profile.objects.filter(user=id).first()
   return user_images
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='picture_folder/')
+  image_name = models.CharField(max_length=25)
+  image_caption = models.CharField(max_length=400, blank=True)
+  post_date = models.DateTimeField(auto_now_add=True)
+  image_likes = models.IntegerField(default=0)
+  author = models.ForeignKey(User,on_delete=models.CASCADE, default='1')
+  profile = models.ForeignKey(Profile,on_delete=models.CASCADE, default='1')
+  
+  def save_image(self):
+    self.save()
+  
+  def __str__(self):
+    return f'{self.profile.user.username}'
+  
+  @classmethod
+  def get_image(request, id):
+    try:
+      image = Image.objects.get(pk=id)
+    except ObjectDoesNotExist:
+      raise Http404()
+    return image
+  
+  @classmethod
+  def get_author(cls, search_term):
+    image = cls.objects.filter(author__username__icontains=search_term)
+    return image
+  
+  class Meta:
+    ordering = ['-post_date']
+    
+  def update_image(self):
+    self.update_image()
+  
+  def update_caption(self, caption):
+    self.image_caption = caption
+    self.save()
+    
+  def delete_image(self):
+    self.delete()
+    
+  @classmethod
+  def get_images(cls):
+    images = cls.objects.all()
+    return images
+  
+  @classmethod
+  def get_profile_images(cls, profile):
+    user_images = Image.objects.filter(profile__id=profile)
+    return user_images
+    
+    
